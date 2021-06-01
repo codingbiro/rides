9ddda60479cd3245971e16c27e1ab6cefb8a5e8c
Vince Biro <hello@birovince.com>
2021-06-01 22:50:05 +0200

    Search and create return rides

--- app.py
+++ app.py
@@ -9,8 +9,29 @@ class Ride:
 
 rides = []
 
+def printRides(elements):
+  print("There is 1 ride stored" if len(elements) == 1 else "There are " + str(len(elements)) + " rides stored")
+  for element in elements:
+    print(element)
+
 def createRide(fromCity, toCity, date, nSeats):
   rides.append(Ride(fromCity, toCity, date, nSeats))
+
+def createReturnRide(date):
+  fromRide = rides[len(rides) - 1]
+  rides.append(Ride(fromRide.toCity, fromRide.fromCity, date, fromRide.nSeats))
+
+def search(fromCity = "", toCity = "", fromDate = "", toDate = "", minFreeSeats = ""):
+  results = []
+  for ride in rides:
+    cityCondition = (not bool(fromCity) or ride.fromCity == fromCity) and (not bool(toCity) or ride.toCity == toCity)
+    dateCondition = (not bool(fromDate) or ride.date >= fromDate) and (not bool(toDate) or ride.date <= toDate)
+    if (not bool(toDate)):
+      dateCondition = not bool(fromDate) or ride.date == fromDate
+    freeSeatsCondition = not bool(minFreeSeats) or ride.nSeats >= minFreeSeats
+    if cityCondition and dateCondition and freeSeatsCondition:
+      results.append(ride)
+  printRides(results)
 
 def main():
   print("Rides by Vince Biro")
@@ -23,19 +44,27 @@ def main():
   exit = False
   while not exit:
     command = input("Your command: ")
-    split = command.split()
+    splitCommand = command.split()
     if command == "q":
       exit = True
     elif command == "ls":
-      print("There is 1 ride stored" if len(rides) == 1 else "There are " + str(len(rides)) + " rides stored")
-      for ride in rides:
-        print(ride, end = '\n')
-    elif split[0] == "C" and len(split) == 5:
-      createRide(split[1], split[2], split[3], split[4])
-    elif split[0] == "R" and len(split) == 2:
-      raise NotImplementedError
-    elif split[0] == "S":
-      raise NotImplementedError
+      printRides(rides)
+    elif splitCommand[0] == "C" and len(splitCommand) == 5:
+      createRide(splitCommand[1], splitCommand[2], splitCommand[3], splitCommand[4])
+    elif splitCommand[0] == "R" and len(splitCommand) == 2:
+      createReturnRide(splitCommand[1])
+    elif splitCommand[0] == "S" and len(splitCommand) == 2:
+      search(splitCommand[1])
+    elif splitCommand[0] == "S" and len(splitCommand) == 3:
+      search(splitCommand[1], splitCommand[2])
+    elif splitCommand[0] == "S" and len(splitCommand) == 4:
+      search(splitCommand[1], splitCommand[2], splitCommand[3])
+    elif splitCommand[0] == "S" and len(splitCommand) == 5:
+      search(splitCommand[1], splitCommand[2], splitCommand[3], splitCommand[4])
+    elif splitCommand[0] == "S" and len(splitCommand) == 6:
+      search(splitCommand[1], splitCommand[2], splitCommand[3], splitCommand[4], splitCommand[5])
+    elif splitCommand[0] == "S" and len(splitCommand) == 7:
+      search(splitCommand[1], splitCommand[2], splitCommand[3], splitCommand[4], splitCommand[5], splitCommand[6])
   print("Exiting...")
 
 main()
